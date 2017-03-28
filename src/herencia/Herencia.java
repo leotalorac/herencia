0ecc8aab4ecb685245a8f671f59b74855602162e
Estudiante <Estudiante@10.203.1.20>
2017-03-28 14:46:59 -0500

    ejemplo de herencia

---
+++ src/herencia/Herencia.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package herencia;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Herencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       B ejemplo = new B(1,25);
+       ejemplo.xx();
+        System.out.println(ejemplo.getA1());
+    }
+    
+}
